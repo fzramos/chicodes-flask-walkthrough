f2abc0a3cf8d715a5656864c8f4a5b657bf02b09
fzramos <64625115+fzramos@users.noreply.github.com>
2020-10-26 14:24:12 -0500

    Fininshing hello world

---
+++ config.py
@@ -0,0 +1,8 @@
+# import the os module
+import os
+
+# creation of base directory for application
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Windows = Documents/chicodes_sept2020/week_5/in_class
+# Mac & Linus = Documents/chicodes_sept2020/week_5/in_class
