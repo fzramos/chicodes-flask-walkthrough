6f490c608ad5cc720f38c9654b202b793de8b67d
fzramos <64625115+fzramos@users.noreply.github.com>
2020-10-27 14:27:23 -0500

    adding server terminal output for form

--- chicodes_blog_app/routes.py
+++ chicodes_blog_app/routes.py
@@ -2,7 +2,7 @@
 from chicodes_blog_app import app
 
 # Import  specific packages from flask
-from flask import render_template
+from flask import render_template, request
 
 # Import user created form class
 from chicodes_blog_app.forms import UserInfoForm
@@ -24,5 +24,18 @@ def testRoute():
 def register():
     # Init our form
     form = UserInfoForm()
+
+
+    # Validate of our form
+    if request.method == 'POST' and form.validate():
+        # Get Information from the form
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        # print the data to the server that comes from the form
+        print(username, email, password)
+
     return render_template('register.html', user_form = form)
     # user_form is just variable name, nothing special, not keywork
+
+    
