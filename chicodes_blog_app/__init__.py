7eb1ad0eb0df5bc3377ea4433dda07dc9cc414af
fzramos <64625115+fzramos@users.noreply.github.com>
2020-10-27 17:14:15 -0500

    Adding database, not connnecting it to form yet -FR

--- chicodes_blog_app/__init__.py
+++ chicodes_blog_app/__init__.py
@@ -3,8 +3,22 @@ from flask import Flask
 #  Import config object
 from config import Config
 
+# Import for the SQLAlchemy Object
+from flask_sqlalchemy import SQLAlchemy
+
+# Import the Migrate object
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 # complete config cycle for our flask app
 # and give accesss to our Database(when we have one)
 # along with our Secret Key
 app.config.from_object(Config)
+
+# Init our database (db)
+db = SQLAlchemy(app)
+
+# Init the migrator
+migrate = Migrate(app, db)
+
+from chicodes_blog_app import routes, models
--- config.py
+++ config.py
@@ -12,3 +12,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # secret key for the encryption of our submitted forms
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you will never guess this...'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Uri is universal relation something
+    # want sqlalchemy to set up or connect to db here called app.db, in root directory
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # stops module from notifying us everytime database is updated
